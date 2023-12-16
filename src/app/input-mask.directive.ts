b1316a30e64a32e7bef39f5a6803b074d07d3ecc
kulapinpavel <kulapinpavel@gmail.com>
2023-12-16 12:45:47 +0500

    bug fixes + clipboard support

--- src/app/input-mask.directive.ts
+++ src/app/input-mask.directive.ts
@@ -1,5 +1,5 @@
 import {Directive, ElementRef, HostListener, Input, OnInit, Optional} from '@angular/core';
-import {AbstractControl, ControlValueAccessor, NgControl} from "@angular/forms";
+import {ControlValueAccessor, NgControl} from "@angular/forms";
 
 @Directive({
   selector: 'input[inputMask]',
@@ -27,7 +27,6 @@ export class InputMaskDirective implements OnInit, ControlValueAccessor {
   }
 
   ngOnInit(): void {
-    console.log(this.inputMaskOnlyNumbers);
     if(!this.inputMask) throw new Error("InputMask directive requires mask value");
     this.value = this.inputMask;
     this.el.nativeElement.value = this.value;
@@ -35,9 +34,9 @@ export class InputMaskDirective implements OnInit, ControlValueAccessor {
 
   @HostListener("keydown", ['$event'])
   onKeydown(e: KeyboardEvent): void {
-    if(e.key.length===1 || !this._allowedKeys.includes(e.key)) e.preventDefault();
+    if(e.key.length===1 && !e.ctrlKey || e.key.length!==1 && !this._allowedKeys.includes(e.key)) e.preventDefault();
 
-    if(e.key.match(/^[a-zA-Z0-9а-яА-Я]$/)) {
+    if (e.key.match(/^(\w|\d)$/) && !e.ctrlKey) {
       this._applyChanges(this._processMaskInput(e.key, this.el.nativeElement.selectionStart));
     } else switch (e.key) {
       case 'Backspace':
@@ -49,39 +48,51 @@ export class InputMaskDirective implements OnInit, ControlValueAccessor {
     }
   }
 
+  @HostListener("paste", ['$event'])
+  onPaste(e: ClipboardEvent): void {
+    e.preventDefault();
+    const clipboardString = e.clipboardData?.getData('text').replaceAll(new RegExp(/(\W|\D)/, 'g'), "");
+    console.log(clipboardString)
+    if(clipboardString) {
+      for (let i = 0; i < clipboardString.length; i++) {
+        this._applyChanges(this._processMaskInput(clipboardString.charAt(i), this.el.nativeElement.selectionStart));
+      }
+    }
+  }
+
   private _processMaskInput(key: string, position: number): number {
     let replacementChar;
     if(Number(key) === +key) replacementChar = this.inputMaskNumberChar;
     else replacementChar = this.inputMaskChar;
 
-    if(this.value.at(position) === replacementChar) {
-      const maskKeyPosition = this.value?.indexOf(replacementChar, position);
+    if(this.inputMask?.at(position) === replacementChar) {
+      const maskKeyPosition = this.inputMask?.indexOf(replacementChar, position);
       if (maskKeyPosition > -1) {
         this.value = this._replaceAt(this.value, key, maskKeyPosition);
       }
-      const nextNumberMask = this.value.substring(position).indexOf(this.inputMaskNumberChar);
-      const nextCharMask = this.value.substring(position).indexOf(this.inputMaskChar);
+      const nextNumberMask = this.inputMask?.substring(position + 1).indexOf(this.inputMaskNumberChar) + 1;
+      const nextCharMask = this.inputMask?.substring(position + 1).indexOf(this.inputMaskChar) + 1;
 
-      if (nextNumberMask > -1 && nextCharMask > -1) return position + Math.min(nextNumberMask, nextCharMask);
-      if (nextNumberMask > -1) return position + nextNumberMask;
-      if (nextCharMask > -1) return position + nextCharMask;
+      if (nextNumberMask > 0 && nextCharMask > 0) return position + Math.min(nextNumberMask, nextCharMask);
+      if (nextNumberMask > 0) return position + nextNumberMask;
+      if (nextCharMask > 0) return position + nextCharMask;
       return position + 1;
     }
     return position;
   }
 
-  private _processMaskErase(selectionStart: number, selectionEnd: number, backspase: boolean): number {
+  private _processMaskErase(selectionStart: number, selectionEnd: number, backspace: boolean): number {
     let positionStart = selectionStart;
     let positionEnd = selectionEnd;
     if(positionStart === positionEnd) {
-      if(backspase) positionStart--;
+      if(backspace) positionStart--;
       else positionEnd++;
     }
     const maskSubstring = this.inputMask?.substring(positionStart, positionEnd);
     if(maskSubstring) {
       this.value = this._replaceAt(this.value, maskSubstring, positionStart);
     }
-    return Math.max(backspase ? positionStart : positionEnd, 0);
+    return Math.max(backspace ? positionStart : positionEnd, 0);
   }
 
   private _replaceAt(source: string, char: string, index: number): string {
